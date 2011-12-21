3001d73ca62e518c40cca46b26841a734d710ef8
Chris Matthews <chris4000@gmail.com>
2011-12-21 15:16:24 -0800

    removed dynamic link stuff as it did not work.

--- lind.sh
+++ lind.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python2.6 $REPY_PATH/repy.py --safebinary $REPY_PATH/restrictions.lind $REPY_PATH/dylink.repy $REPY_PATH/lind_server.py $@
+set -o xtrace
+python2.6 $REPY_PATH/repy.py --safebinary $REPY_PATH/restrictions.lind $REPY_PATH/lind_server.py $@
