756da2b1e7034facd131e35801665f194e11c718
Chris Matthews <chris4000@gmail.com>
2012-01-06 09:47:37 -0800

    changed to use new install paths

--- lind.sh
+++ lind.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -o xtrace
-python2.6 $REPY_PATH/repy.py --safebinary $REPY_PATH/restrictions.lind $REPY_PATH/lind_server.py $@
+python2.6 $REPY_PATH/repy/repy.py --safebinary $REPY_PATH/repy/restrictions.lind $REPY_PATH/repy/lind_server.py $@
