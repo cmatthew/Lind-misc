3f669c3a6eef5bbb30fab9688375debb019a4d21
Paul Demchuk <pademchuk@gmail.com>
2012-04-26 13:01:46 -0700

    commented performance code

--- benchmark/paul/memoryusage/addTimes.py
+++ benchmark/paul/memoryusage/addTimes.py
@@ -4,35 +4,47 @@ import sys
 
 def main():
 	
+	#argument 1 is the filename you want to add the times for
+	#argument 2 is either 1 or 0. If it is 1, then you will see WHAT COMMANDS were retrieved from the ps call
+	#so you would see something like:
+	#    30000 python2.6 python2.6 etc
+	#*************THE SECOND ARGUMENT SHOULD BE 0 IF YOU PLAN ON PUTTING THE DATA INTO MATLAB***************
 	if(sys.argv[1] == "" or sys.argv[2] == ""):
 		print "usage: ./addTimes.py <filename> <show CMD names 1 or 0>"
 		exit(0)
 	
 	showCMD = int(sys.argv[2])
 		
-	f = open(sys.argv[1],'r')
+	f = open(sys.argv[1],'r')#who needs error checking?
 	
 	total = 0
-	cmdList = []
-	time = '';
+	cmdList = []#will hold the commands of the current PS (ONLY IF arg2 = 1)!!
+	time = '';#will hold the start time
 	for line in f.readlines():
+		#I deliminate ps' by 5 ='s, so once we see the deliminator, we know we have a new ps
 		if not line.strip() == "=====":
+			#timestamps start with t, if it is anyting else, that means we are showing commands
 			if line[0] == 't':
 				time = line[1:]
 			else:
+				#split the command, add to list
 				lineSplit = line.split()
 				if not lineSplit[0] == "":
 					total += int(lineSplit[0])
 				if not lineSplit[1] == "":
 					cmdList.append(lineSplit[1] + ":::" + lineSplit[2])
 
-			
+		#once we see the =====, we want to print out the total of the previous ps
 		else:
+			#dont worry if total is 0
 			if not total == 0:
+				#if we are showing commands, we want to show the total, and then the processes that made up that total
 				if showCMD:
 					print "Time: " + repr(total) + ", commands: " + repr(cmdList)
+				#else just print the total time and start time!
 				else:
 					print repr(total) + "," +time.strip()
+			#reset values
 			total = 0
 			cmdList = []
 			time=""
--- benchmark/paul/memoryusage/performace_test.sh
+++ benchmark/paul/memoryusage/performace_test.sh
@@ -3,16 +3,16 @@ ITTER=1000
 
 rm $log
 
-#if a 1 is passed in, 
+#if a 1 is passed in, we are getting NATIVE times
 if test $1 -eq 1
 then 
-	echo "Getting native-grep memory Usage"
+	echo "Getting native-grep memory Usage" #helpful message
 	for((i = 0; i < $ITTER; i++))
 		do
-			echo "t"`date +%s.%N` >> $log
-			ps -U lind -o rss=MEM,comm=CMD,pid=PID | grep -E "grep" >> $log		
-			echo "=====" >> $log
-			sleep .1
+			echo "t"`date +%s.%N` >> $log #get the timestamp of NOW
+			ps -U lind -o rss=MEM,comm=CMD,pid=PID | grep -E "grep" >> $log #get all the processes of the ps. Since it is native, we are just looking for a grep
+			echo "=====" >> $log #deliminator
+			sleep .1 #we dont want to just blitz, because then we will get multiples of the SAME process ID, with different ps'
 			echo $i
 		done
 
@@ -20,13 +20,14 @@ then
 
 	./addTimes.py $log 1 > bigPrintOutLind
 
+#if a 0 is passed in, we are getting LIND times
 elif test $1 -eq 0
 then
 	echo "Getting lind-grep memory Usage"
 	for((i = 0; i < $ITTER; i++))
 		do	
 			echo "t"`date +%s.%N` >> $log
-			ps -U lind -o rss=MEM,comm=CMD,pid=PID | grep -E "lind|python2.6|sel_ldr" >> $log		
+			ps -U lind -o rss=MEM,comm=CMD,pid=PID | grep -E "lind|python2.6|sel_ldr" >> $log #get processes, since it is lind, we are looking for 3 things!
 			echo "=====" >> $log
 			sleep .1
 			echo $i
